0f93bfecb45fc56fd23ca14c3349d45d7e05d900
Tumble <CMPCTROW@ljmu.ac.uk>
2018-04-12 12:04:04 +0100

    added constrctor initialisation

---
+++ Coursework/2D Computer Graphics Assignment/SFML_AnimatedSpriteObject.cpp
@@ -0,0 +1,41 @@
+#include "SFML_AnimatedSpriteObject.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+SFML_AnimatedSpriteObject::SFML_AnimatedSpriteObject()
+	: m_currentAnimation(NULL)
+	, m_isPaused(false)
+	, m_currentAnimationID(0)
+{}
+
+
+SFML_AnimatedSpriteObject::~SFML_AnimatedSpriteObject()
+{}
+
+int SFML_AnimatedSpriteObject::addAnimation(std::string spriteSheetFilename, std::string spriteInfoFilename, float animationTime)
+{
+	return 0;
+}
+
+void SFML_AnimatedSpriteObject::setCurrentAnimation(int animationIndex)
+{}
+
+SFML_SpriteAnimation * SFML_AnimatedSpriteObject::getCurrentAnimation()
+{
+	return nullptr;
+}
+
+void SFML_AnimatedSpriteObject::update(float elapsedTime)
+{}
+
+void SFML_AnimatedSpriteObject::play()
+{}
+
+void SFML_AnimatedSpriteObject::pause()
+{}
+
+void SFML_AnimatedSpriteObject::stop()
+{}
