22971cdedab3c623cb578fffed59f15749f366a5
Tumble <CMPCTROW@ljmu.ac.uk>
2018-04-12 11:59:18 +0100

    added private atributes

--- Coursework/2D Computer Graphics Assignment/SFML_AnimatedSpriteObject.h
+++ Coursework/2D Computer Graphics Assignment/SFML_AnimatedSpriteObject.h
@@ -22,6 +22,12 @@ public:
 	void pause();
 	void stop();
 
+private:
+	std::vector<sf::Texture*> m_spriteTextureList;
+	int m_currentAnimationID;
+	float m_currentTime;
+	bool m_isPaused;
+
 protected:
 	std::vector <SFML_AnimatedSpriteObject*> m_animatedSpriteList;
 	SFML_SpriteAnimation* m_currentAnimation;
