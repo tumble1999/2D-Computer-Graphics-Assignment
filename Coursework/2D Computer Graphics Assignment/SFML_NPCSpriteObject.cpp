b9e64aa05ea4d59a0a160f95de8e4461f9f08a02
Tumble <CMPCTROW@ljmu.ac.uk>
2018-04-19 09:58:10 +0100

    stuff

---
+++ Coursework/2D Computer Graphics Assignment/SFML_NPCSpriteObject.cpp
@@ -0,0 +1,71 @@
+#include "SFML_NPCSpriteObject.h"
+
+
+SFML_NPCSpriteObject::SFML_NPCSpriteObject()
+	: m_targetLocation(0, 0)
+	, m_moveSpeed(100.0f)
+	, m_state(IDLE)
+	, m_idleAnimationID(-1)
+	, m_walkingAnimationID(-1)
+	, m_attackingAnimationID(-1)
+	, m_visionConeAngle(90.0f)
+	, m_visionRange(1200.0f)
+{
+
+}
+
+SFML_NPCSpriteObject::~SFML_NPCSpriteObject()
+{
+}
+
+int SFML_NPCSpriteObject::setIdleAnimation(std::string spriteSheetFilename, std::string spriteInfoFilename, float animationTime)
+{
+	m_idleAnimationID = addAnimation(spriteSheetFilename, spriteInfoFilename, animationTime);
+	return m_idleAnimationID;
+}
+
+int	SFML_NPCSpriteObject::setWalkingAnimation(std::string spriteSheetFilename, std::string spriteInfoFilename, float animationTime)
+{
+	m_walkingAnimationID = addAnimation(spriteSheetFilename, spriteInfoFilename, animationTime);
+	return m_walkingAnimationID;
+}
+
+int SFML_NPCSpriteObject::setAttackingAnimation(std::string spriteSheetFilename, std::string spriteInfoFilename, float animationTime)
+{
+	m_attackingAnimationID = addAnimation(spriteSheetFilename, spriteInfoFilename, animationTime);
+	return m_attackingAnimationID;
+}
+
+void SFML_NPCSpriteObject::toIdleState()
+{
+	m_state = IDLE;
+	setCurrentAnimation(m_idleAnimationID);
+}
+
+void SFML_NPCSpriteObject::toWalkingState()
+{
+	m_state = WALKING;
+	setCurrentAnimation(m_walkingAnimationID);
+}
+
+void SFML_NPCSpriteObject::toAttackingState()
+{
+	m_state = ATTACKING;
+	setCurrentAnimation(m_attackingAnimationID);
+}
+
+void SFML_NPCSpriteObject::setTargetLocation(sf::Vector2f targetLocation)
+{
+	m_targetLocation = targetLocation;
+}
+
+void SFML_NPCSpriteObject::setMovementSpeed(float speed)
+{
+	m_moveSpeed = speed;
+}
+
+void SFML_NPCSpriteObject::update(float elapsedTime)
+{
+
+	SFML_AnimatedSpriteObject::update(elapsedTime);
+}
---
+++ Coursework/2D Computer Graphics Assignment/SFML_NPCSpriteObject.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "SFML_AnimatedSpriteObject.h"
+class SFML_NPCSpriteObject :
+	public SFML_AnimatedSpriteObject
+{
+public:
+			SFML_NPCSpriteObject();
+	virtual ~SFML_NPCSpriteObject();
+
+	virtual void	update(float elapsedTime);
+
+	void	setTargetLocation(sf::Vector2f targetLocation);
+	void	setMovementSpeed(float speed);
+
+	int		setIdleAnimation(std::string spriteSheetFilename, std::string spriteInfoFilename, float animationTime);
+	int		setWalkingAnimation(std::string spriteSheetFilename, std::string spriteInfoFilename, float animationTime);
+	int		setAttackingAnimation(std::string spriteSheetFilename, std::string spriteInfoFilename, float animationTime);
+
+	void	toIdleState();
+	void	toWalkingState();
+	void	toAttackingState();
+
+private:
+
+	enum NPCState {
+		IDLE,
+		WALKING,
+		ATTACKING
+	};
+
+	NPCState		m_state;
+	
+	sf::Vector2f	m_targetLocation;
+	float			m_moveSpeed;
+	int				m_idleAnimationID;
+	int				m_walkingAnimationID;
+	int				m_attackingAnimationID;
+
+	float			m_visionConeAngle;
+	float			m_visionRange;
+};
+
